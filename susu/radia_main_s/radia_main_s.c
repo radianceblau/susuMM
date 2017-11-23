c9f914d756210c8ce4c561c2e35bc13c2b13d6f6
radia <radianceblau@163.com>
2017-11-23 22:51:41 +0800

    [susu]:modify radia_main_s and complete it as jz2440_show

--- susu/radia_main_s/radia_main_s.c
+++ susu/radia_main_s/radia_main_s.c
@@ -3,74 +3,137 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+#include "../../include/radia_comm.h"
 
-//------------------------------------------------------socket--------------------------------------------------------------------------
-#define DISPLAY_PORT_NUM	9990
-#define INPUT_PORT_NUM		9991
-#define MAX_CLIENT_NAME_LEN 100		//每个client上报name的最大字节数
-#define MAX_MSG_LEN	100
+#define SCREEN_COLOR_RED	0xF800
+#define SCREEN_COLOR_GREEN	0x07E0
+#define SCREEN_COLOR_BLUE	0x001F
 
-struct st_radia_msg  
-{  
-    char msg_type;  
-    char text[MAX_MSG_LEN];  
-};
+int display_csd;//display_csd;
+int ntp_ok_flag = 1;//0代表成功，整数代表失败的次数
 
-int client_socket_descriptor;
-void init_socket_client()
+//0x10 pure
+//0x11 ascii
+//0x12 word mei
+void send_display_msg(int tpye, int x, int y, int multiple, int num, int bg_color, int word_color)
 {
-	struct sockaddr_in s_addr;
-	unsigned short portnum = DISPLAY_PORT_NUM;
-	
-	client_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
-	if(-1 == client_socket_descriptor)
-	{
-		printf("socket fail ! \r\n");
-		return -1;
-	}
-	printf("socket ok !\r\n");
-
-	bzero(&s_addr,sizeof(struct sockaddr_in));
-	s_addr.sin_family=AF_INET;
-	s_addr.sin_addr.s_addr= inet_addr("192.168.31.200");
-	s_addr.sin_port=htons(portnum);
-	printf("s_addr = %#x ,port : %#x\r\n",s_addr.sin_addr.s_addr,s_addr.sin_port);
-
-	if(-1 == connect(client_socket_descriptor,(struct sockaddr *)(&s_addr), sizeof(struct sockaddr)))
-	{
-		printf("connect fail !\r\n");
-		return -1;
-	}
-	printf("connect ok !\r\n");
+	struct st_display_msg send_msg;
+	memset(&send_msg, 0, sizeof(struct st_display_msg));
+	send_msg.type = tpye;
+	send_msg.x = x;
+	send_msg.y = y;
+	send_msg.multiple = multiple;
+	send_msg.num = num;
+	send_msg.bg_color = bg_color;
+	send_msg.word_color = word_color;		
+	send(display_csd, (void *)(&send_msg), sizeof(struct st_display_msg), 0);
 }
 
-//--------------------------------------------------------end socket------------------------------------------------------------------------
+void show_ntp_state()
+{
+	short ntp_color;
+	if(ntp_ok_flag == 0)
+		ntp_color = SCREEN_COLOR_GREEN;
+	else
+		ntp_color = SCREEN_COLOR_RED;
+
+	send_display_msg(0x11, 200, 0, 4, 46, 0, ntp_color);//N
+	send_display_msg(0x11, 200+32, 0, 4, 52, 0, ntp_color);//T
+	send_display_msg(0x11, 200+32*2, 0, 4, 48, 0, ntp_color);//P
+	send_display_msg(0x11, 200+32*3, 0, 4, 26, 0, ntp_color);//:
+	send_display_msg(0x11, 200+32*4, 0, 4, 16+ntp_ok_flag/100, 0, ntp_color);//9
+	send_display_msg(0x11, 200+32*5, 0, 4, 16+((ntp_ok_flag/10)%10), 0, ntp_color);//9
+	send_display_msg(0x11, 200+32*6, 0, 4, 16+(ntp_ok_flag%10), 0, ntp_color);//9
+}
+void update_time(struct tm *timenow)
+{
+	int ret, update_flag;
+	if(timenow->tm_hour == 0 && timenow->tm_min == 0 && timenow->tm_sec == 0)
+		update_flag = 1;
+	else 
+		update_flag = 0;
+	if(ntp_ok_flag != 0 || update_flag)//每天0点更新，或者失败后每分钟尝试一次
+	{
+		if(timenow->tm_sec == 0)
+		{
+			ret = system("ntpdate 1.cn.pool.ntp.org");
+			if(ret == 0)
+			{
+				ntp_ok_flag = 0;//更新成功
+			}
+			else
+			{
+				ntp_ok_flag++;//更新失败
+				if(ntp_ok_flag >= 999)
+					ntp_ok_flag = 999;
+			}
+			show_ntp_state();
+		}
+	}
+	
+}
+
+struct tm *getNowTime()
+{
+	time_t now;		  //实例化time_t结构
+	struct tm *timenow;		  //实例化tm结构指针
+	time(&now);
+	//time函数读取现在的时间(国际标准时间非北京时间)，然后传值给now
+	timenow = localtime(&now);
+	//localtime函数把从time取得的时间now换算成你电脑中的时间(就是你设置的地区)
+	printf("Local time is %s/n",asctime(timenow));
+	//上句中asctime函数把时间转换成字符，通过printf()函数输出
+	return timenow;
+}
+
 int main()
 {
 	int recbyte;
-	struct st_radia_msg recv_msg, send_msg;
+	struct tm *timenow;
 
 	printf("radia main running!\r\n");
-	init_socket_client();
+	if(system("/radia/radia_display_s > /dev/null &") == 0)
+		printf("start radia_display_s success\n");
+	else
+		printf("start radia_display_s failed!\n");
+	sleep(1);
+	if(system("/radia/radia_web_s > /dev/null &") == 0)
+		printf("start radia_web_s success\n");
+	else
+		printf("start radia_web_s failed!\n");
+	sleep(1);	
+	
+	if(open_client_socket(&display_csd, "127.0.0.1", DISPLAY_PORT_NUM) != 1)		
+	{
+		printf("open_client_socket error!\n");
+	}
+	send_display_msg(0x12, 0, 0, 3, 0, 0, 0xFF00);//mei
 	while(1)
 	{
-		memset(&recv_msg.text, 0, MAX_MSG_LEN);
-		memset(&send_msg.text, 0, MAX_MSG_LEN);
-		//输入数据  
-		printf("Enter some text: ");
-		fgets(send_msg.text, MAX_MSG_LEN, stdin);
-		send_msg.msg_type = 1;
-		send(client_socket_descriptor, (void *)(&send_msg), strlen(send_msg.text) + sizeof(char), 0);
-		
-		if(-1 == (recbyte = read(client_socket_descriptor, (void *)(&recv_msg), sizeof(struct st_radia_msg))))
+		show_ntp_state();
+		if(system("ntpdate 1.cn.pool.ntp.org") == 0)
 		{
-				printf("read data fail !\r\n");
-				return -1;
+			ntp_ok_flag = 0;
+			show_ntp_state();
+			break;
 		}
-		printf("recv:%s\n",recv_msg.text);
+		else
+		{
+			ntp_ok_flag++;
+		}
+	}
+	while(1)
+	{
+		timenow = getNowTime();
+		update_time(timenow);
+		sleep(1);
 	}
 
-	close(client_socket_descriptor);
+	close(display_csd);
 
 	return 0;
 
